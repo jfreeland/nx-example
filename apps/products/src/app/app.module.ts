4d56f6452a841940e436c4c9d6419c0c2a480ad3
Joey Freeland <joey@free.land>
2025-02-06 18:35:33 -0500

    fix: nope

--- apps/products/src/app/app.module.ts
+++ apps/products/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 
-// err closer
+// err close
 
 @NgModule({
   declarations: [AppComponent],
